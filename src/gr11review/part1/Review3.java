00c3bd23a3ffbdb13795d799fc2fa8736ba75727
caleb <duocaleb3@gmail.com>
2024-09-16 10:09:19 -0400

    Review 3 Done

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,12 @@
 package gr11review.part1;
+public class Review3{
+    public static void main(String[] args){
+        for(int i = 21; i < 100; i += 2){
+            System.out.println(i);
+        }
+        System.out.println();
+        for(int j = 29;  j >= 2; j--){
+            System.out.println(j);
+        }
+    }
+}
