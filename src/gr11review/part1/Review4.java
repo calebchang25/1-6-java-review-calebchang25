e18349b53d89414f93831c6302d96db467627e15
caleb <duocaleb3@gmail.com>
2024-09-16 10:28:02 -0400

    Finished Review 4

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,1 +1,19 @@
 package gr11review.part1;
+import java.text.DecimalFormat;
+import java.io.*;
+public class Review4{
+    public static void main(String[] args) throws IOException{
+        DecimalFormat money = new DecimalFormat("$#,##0.00");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("How many items do you want to buy? ");
+        int itemCount = Integer.parseInt(input.readLine());
+        double price = 0;
+        for(int i = 1; i <= itemCount; i++){
+            System.out.print("Enter the price for item " + i + ": ");
+            price += Double.parseDouble(input.readLine());
+        }
+        System.out.println("Subtotal: " + money.format(price));
+        System.out.println("Tax: " + money.format(price*0.13));
+        System.out.println("Total: " + money.format(price*1.13));
+    }
+}
